ccdea0b44fc453acf33f1a9e95d788721206cb96
sandeepkumar23f <sandeep21000552@gmail.com>
2026-06-08 12:07:25 +0530

    learned the useState hook in rn and also using this created a basic fun page

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,62 @@
-import { View, Text} from "react-native"
-import "../global.css"
-type funProp={
-  name: string,
+import { useEffect, useState } from "react";
+import { Text, View, Button } from "react-native";
+
+type CatProps = {
+  name: string
 }
-const Dog = (props: funProp) => {
-  return (
+
+const Cat = (props: CatProps)=>{
+  const [isHungry,setIsHungry]=useState(true)
+  const [timerRunning,setTimerRunning]=useState(false)
+  const [secondsLeft,setSecondsLeft]=useState(60)
+
+  useEffect(()=>{
+    let interval: ReturnType<typeof setInterval >;
+
+    if(timerRunning){
+      interval = setInterval(() => {
+        setSecondsLeft((prev) => {
+          if(prev<=1){
+            clearInterval(interval)
+            setIsHungry(true)
+            setTimerRunning(false)
+            return 60;
+          }
+          return prev-1
+        })
+      },1000)
+    }
+    return () => clearInterval(interval)
+  },[timerRunning])
+
+  const feedCat = () => {
+    setIsHungry(false);
+    setTimerRunning(true);
+    setSecondsLeft(60)
+  }
+  return(
     <View>
-      <Text className="text-center font-bold text-green-600">how are you {props.name}!</Text>
+      <Text>I am {props.name}, and I am {isHungry ? 'hungry': 'full' }!</Text>
+
+      {isHungry ? (
+        <Button title="Give me some food! I am hungry!" onPress={feedCat}/>
+      ):
+       <Text>
+        Thank you! Malik mera pet bhara hai
+        {"\n"}Hungry again in {secondsLeft} seconds
+       </Text>
+      }
     </View>
   )
 }
 
-const DogFunction = () =>{
- return(
-   <View >
-    <Text className="text-center">Hello bro!</Text>
-    <Dog name="Alen"/>
-    <Dog name="Bob" />
-  </View>
- )
-}
+const Cafe = () => {
+  return (
+    <>
+      <Cat name="desi-bili" />
+      <Cat name="videshi-billi" />
+    </>
+  );
+};
 
-export default DogFunction;
+export default Cafe;
